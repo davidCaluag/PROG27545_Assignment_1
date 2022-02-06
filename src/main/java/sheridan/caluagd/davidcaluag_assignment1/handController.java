b500f19822420869b287116bebb2d6b11e5a4777
davidCaluag <caluagd@sheridanc.on.ca>
2022-02-05 20:49:28 -0500

    added hand controller add handroll

---
+++ src/main/java/sheridan/caluagd/davidcaluag_assignment1/handController.java
@@ -0,0 +1,26 @@
+package sheridan.caluagd.davidcaluag_assignment1;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class handController {
+
+    private final Logger logger = LoggerFactory.getLogger(handController.class);
+
+    @GetMapping(value={"/", "/random-hand"})
+    public String hand(Model model){
+        logger.trace("hand() is called");
+
+        handRoll hand = new handRoll();
+        logger.debug("hand = " + hand);
+
+        model.addAttribute("card", hand);
+        return "Hand";
+    }
+
+
+}
