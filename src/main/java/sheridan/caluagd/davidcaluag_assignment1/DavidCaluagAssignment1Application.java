e4a7b1fb9418a72c4cf6cc2bb9006deeb1a9a9a4
davidCaluag <caluagd@sheridanc.on.ca>
2022-02-05 20:28:24 -0500

    started

---
+++ src/main/java/sheridan/caluagd/davidcaluag_assignment1/DavidCaluagAssignment1Application.java
@@ -0,0 +1,13 @@
+package sheridan.caluagd.davidcaluag_assignment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavidCaluagAssignment1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DavidCaluagAssignment1Application.class, args);
+    }
+
+}
---
+++ src/test/java/sheridan/caluagd/davidcaluag_assignment1/DavidCaluagAssignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package sheridan.caluagd.davidcaluag_assignment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DavidCaluagAssignment1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
