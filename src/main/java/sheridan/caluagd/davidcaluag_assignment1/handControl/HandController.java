c850d9c25074974ba23815cd4a71d959abec122c
davidCaluag <caluagd@sheridanc.on.ca>
2022-02-06 01:20:00 -0500

    changing a lot of code

--- src/main/java/sheridan/caluagd/davidcaluag_assignment1/handControl/HandController.java
+++ src/main/java/sheridan/caluagd/davidcaluag_assignment1/handControl/HandController.java
@@ -1,11 +1,14 @@
 package sheridan.caluagd.davidcaluag_assignment1.handControl;
-import org.springframework.web.servlet.ModelAndView;
-import sheridan.caluagd.davidcaluag_assignment1.handRoll.HandRoll;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.ModelAndView;
+import sheridan.caluagd.davidcaluag_assignment1.handRoll.HandRoll;
 
 
 @Controller
@@ -13,23 +16,24 @@ public class HandController {
 
     private final Logger logger = LoggerFactory.getLogger(HandController.class);
 
+
     @GetMapping(value={"/", "/input"})
     public ModelAndView input(){
         logger.trace("input() is called");
-        return new ModelAndView("Hand", "envelope", new HandController());
+        return new ModelAndView("Hand", "hand", new HandRoll());
     }
 
 
-    @GetMapping(value={"/", "/random-hand"})
-    public String hand(Model model){
-        logger.trace("hand() is called");
-
-        HandRoll hand =new HandRoll();
-        logger.debug("hand = " + hand);
-
-
-        model.addAttribute("hand", hand);
-        return "Hand";
+    @GetMapping("/process")
+    public ModelAndView process(
+            @Validated @ModelAttribute HandRoll hand,
+            BindingResult bindingResult){
+        logger.trace("process() is called");
+        logger.debug("handRoll = " + hand);
+        if(bindingResult.hasErrors()){
+            return new ModelAndView("Hand");
+        }
+        return new ModelAndView("Output", "hand", hand);
     }
 
 
--- src/main/java/sheridan/caluagd/davidcaluag_assignment1/handRoll/HandRoll.java
+++ src/main/java/sheridan/caluagd/davidcaluag_assignment1/handRoll/HandRoll.java
@@ -2,70 +2,83 @@
 
 package sheridan.caluagd.davidcaluag_assignment1.handRoll;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public class HandRoll {
+public class HandRoll implements Serializable {
 
     private final static String[] _hand
             = {"Rock", "Paper", "Scissors"};
 
-    public String personHand;
-    public String computerHand;
-    public String _answer;
-    public String _message;
+
+    private String personHand;
+    private String computerHand;
+    private String _answer;
+    private int _messsage=0;
 
     public HandRoll() {
+    };
+
+    public int getMessage(){
+        return _message;
     }
 
+
     public String getComputer() {
+        return computerHand;
+    }
+
+    public void setComputer() {
         int computerRand = (int) (2 * Math.random());
         this.computerHand = _hand[computerRand];
-        return computerHand;
     }
 
     public String getPerson() {
         return personHand;
     }
 
-    public void setPerson(String userHand) {this.personHand = userHand;};
+    public void setPerson(String userHand) {
+        this.personHand = userHand;
+    }
+
+    ;
 
 
-    public void setAnswer(){
-        _answer = findAnswer(personHand,computerHand);
+    public void setAnswer() {
+        _answer = findAnswer(personHand, computerHand);
     }
 
     public String getAnswer() {
-        _answer = findAnswer(personHand,computerHand);
-        return _answer;
+        return findAnswer(personHand, computerHand);
+
     }
 
-    public String findAnswer(String answer, String compare){
+    public String findAnswer(String answer, String compare) {
         String returnAnswer = null;
-        if(!Objects.equals(answer, compare)){
-            switch(answer){
+        if (!Objects.equals(answer, compare)) {
+            switch (answer) {
                 case "Rock":
-                    returnAnswer = Objects.equals(compare, "Paper")? "Lost" : "Won";
+                    returnAnswer = Objects.equals(compare, "Paper") ? "Lost" : "Won";
                     break;
                 case "Scissors":
-                    returnAnswer = Objects.equals(compare, "Rock")? "Lost" : "Won";
+                    returnAnswer = Objects.equals(compare, "Rock") ? "Lost" : "Won";
                     break;
                 case "Paper":
-                    returnAnswer = Objects.equals(compare, "Scissors")? "Lost" : "Won";
+                    returnAnswer = Objects.equals(compare, "Scissors") ? "Lost" : "Won";
                     break;
             }
-        }
-        else{
+        } else {
             returnAnswer = "tied";
-        };
-    return returnAnswer;
+        }
+        return returnAnswer;
     }
 
     @Override
     public String toString() {
-        return "Envelope{" +
-                "message=" + message +
-                ", registered=" + registered +
+        return "HandRoll{" +
+                "personHand=" + personHand +
+                "computerHand=" + computerHand +
                 '}';
     }
-    }
+}
 
